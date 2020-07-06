52adf99e4ae664c3668827d0f3cf1ce42de2eb99
Selena <selenawalshsmith@gmail.com>
2020-07-05 19:43:58 -0700

    Front end mostly done

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import Nav from "./components/layout/Nav";
 import Explore from "./components/Explore";
 import Book from "./components/Book";
 import UserHome from "./components/UserHome";
-//import Register from "./components/authentication/Register";
-//import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
+import Login from "./components/authentication/Login";
 
 //import setAuthToken from "./actions/setAuthToken";
 import jwt_decode from "jwt-decode";
@@ -20,8 +20,8 @@ class App extends Component {
           <Switch>
           <Route path="/" exact component={UserHome}/>
           <Route path="/explore" exact component={Explore}/>
-          {/*<Route path="/register" exact component={Register}/>*/}
-          {/* <Route path="/login" exact component={Login}/> */}
+          <Route path="/register" exact component={Register}/>
+          <Route path="/login" exact component={Login}/>
           {/* <Route path="/book" component={Book}/> */}
           </Switch>
         </div>
