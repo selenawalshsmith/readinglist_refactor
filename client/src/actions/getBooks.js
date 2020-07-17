49aef5fe43ae56a024c4091dcdd0733879ad4d0c
Selena <selenawalshsmith@gmail.com>
2020-07-16 21:36:29 -0700

    x

--- client/src/actions/getBooks.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const getBooks = (email) => {
-  axios.post("/api/users/getBookList", email).then(res=> {
-    console.log(res.data.bookList);
-  }).catch(err => {console.log(err);});
-}
-export default getBooks;
